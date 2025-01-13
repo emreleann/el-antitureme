85c22476c218e75c67fe7ae20d4c92f60a0dd377
Emre <84708247+emreleann@users.noreply.github.com>
2025-01-13 17:32:00 +0300

    readme upd

--- el-antitureme/client.lua
+++ el-antitureme/client.lua
@@ -2,23 +2,23 @@ QBCore = exports['qb-core']:GetCoreObject()
 
 
 RegisterNetEvent("dri:pencere", function(display)
-  -- NUI mesajını gönder
+  
   SendNUIMessage({
-      type = "ui", -- NUI tipi
-      display = display -- NUI'nin görüntülenip görüntülenmeyeceği
+      type = "ui", 
+      display = display 
   })
 
-  -- Eğer display true ise UI'yi aktif et
+  
   if display then
-      SetNuiFocus(false, false) -- NUI'ye fokuslan
+      SetNuiFocus(false, false)
   else
-      SetNuiFocus(false, false) -- Fokus kaldır
+      SetNuiFocus(false, false)
       QBCore.Functions.Notify("Hoş değil, bir daha olmasın gülüm" , "error")
   end
 end)
 
 RegisterNetEvent('qb-input:showInput', function()
-  -- Input penceresini göster
+ 
   local result = exports['qb-input']:ShowInput({
       header = "ID gir",
       submitText = "Onayla",
@@ -32,10 +32,10 @@ RegisterNetEvent('qb-input:showInput', function()
       }
   })
 
-  -- Eğer bir ID girildiyse
+  
   if result then
       local id = result.id
-      -- ID'yi kullanarak belirtilen oyuncuya event gönder
+     
       TriggerServerEvent("dri:triggerPencere", id)
       QBCore.Functions.Notify("Yolladım lavuga", "success")
   end
